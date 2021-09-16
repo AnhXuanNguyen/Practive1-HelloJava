85c9737eb4ede88b3b40b4b0535ca27d4a9b1b50
AnhXuanNguyen <fr13nd94@gmail.com>
2021-09-16 11:55:56 +0700

    update test day in month

--- src/Practive1.java
+++ src/Practive1.java
@@ -58,7 +58,27 @@ public class Practive1 {
             }
         }
 
-
-        
+        //Các tháng có 30 ngày hay 31 ngày
+        System.out.println("Nhập vào số tháng");
+        int month = scanner.nextInt();
+        String daysInMonth;
+        switch (month){
+            case 2: daysInMonth = "28 or 29"; break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12: daysInMonth = "31"; break;
+            case 4:
+            case 6:
+            case 9:
+            case 11: daysInMonth = "30"; break;
+            default: daysInMonth = "";
+        }
+        if (!daysInMonth.equals("")) System.out.printf("The month '%d' has %s days!", month, daysInMonth);//Nếu chuỗi daysInMonth khác với chuỗi ""(rỗng) tức là không rơi vào trường hợp default
+        //println để in ra text, printf để in ra text có kèm biến gán biến số nguyên month vào %d và biến chuỗi daysInMonth vào %s
+        else System.out.print("Invalid input!");
     }
 }
